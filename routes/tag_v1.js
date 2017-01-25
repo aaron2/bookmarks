dbab241e9fe20b78a8ed8602363f10ca3ef88614
root <aaronf@gmail.com>
2017-01-25 00:56:56 -0600

    add tag completion endpoint

--- routes/tag_v1.js
+++ routes/tag_v1.js
@@ -36,3 +36,37 @@ exports.count = function(req, res) {
     }
   });
 };
+
+exports.complete = function(req, res) {
+  var viewOpts = { group: true };
+  var view = undefined;
+  if (req.session.user) {
+    view = 'taglist/user';
+    viewOpts.endkey =  [ req.session.user, {} ];
+    viewOpts.startkey = [ req.session.user ];
+  } else {
+    view = 'taglist/public';
+  }
+  global.db.bookmarks.view(view, viewOpts, function(err, rows) {
+    var matches = [];
+    var n = req.query.n || 10;
+    var q = req.query.q || '';
+    for (t in rows) {
+      if (rows[t].key[1].indexOf(q) == 0) {
+        matches.push(rows[t]);
+      }
+    }
+    var out = [];
+    matches.sort(tagSort).some(function(t) {
+      out.push(t['key'][1]);
+      if (out.length >= n) return true;
+    });
+    res.send(out);
+  });
+};
+
+function tagSort(a, b) {
+  if (a.value > b.value) return -1;
+  if (a.value < b.value) return 1;
+  return 0
+}
